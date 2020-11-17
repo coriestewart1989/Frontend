41f9edf7f9342727e11f5915c82d03268233e493
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-11-16 20:21:41 -0500

    merge

--- application/src/components/Toggle.js
+++ application/src/components/Toggle.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-// import "../styles/ToggleSwitch.scss";
+import "../styles/ToggleSwitch.scss";
 
 export default function Toggle({
   id,
